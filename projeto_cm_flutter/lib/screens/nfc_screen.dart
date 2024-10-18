40dd0adf192f0ea66c42046e7ab19de809f6fc63
Gui <guicostaantunes@gmail.com>
2024-10-18 18:28:08 +0100

    idk

--- projeto_cm_flutter/lib/screens/nfc_screen.dart
+++ projeto_cm_flutter/lib/screens/nfc_screen.dart
@@ -106,29 +106,6 @@ class _NFCScreenState extends State<NFCScreen> {
     super.dispose();
   }
 
-  Widget _buildContainer() {
-    return Container(
-        margin:
-            EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0, bottom: 20.0),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(1),
-              spreadRadius: 0,
-              blurRadius: 5,
-              offset: const Offset(10, 10), // changes position of shadow
-            ),
-          ],
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30.0),
-          child: Image.asset(
-            'assets/images/card.png',
-          ),
-        ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -139,10 +116,27 @@ class _NFCScreenState extends State<NFCScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Flexible(
-              child: Draggable(
-                  feedback: _buildContainer(),
-                  childWhenDragging: Container(),
-                  child: _buildContainer())),
+              child: Container(
+                  margin: EdgeInsets.only(
+                      top: 20.0, left: 20.0, right: 20.0, bottom: 20.0),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(1),
+                        spreadRadius: 0,
+                        blurRadius: 5,
+                        offset:
+                            const Offset(10, 10), // changes position of shadow
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image.asset(
+                      'assets/images/card.png',
+                    ),
+                  ))),
           Container(
             padding: const EdgeInsets.all(20.0),
             child: Text(
