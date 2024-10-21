872046821c86892772103debc4b445894aa5d5c3
r4sinhas <rasinhas@ua.pt>
2024-10-21 16:16:21 +0100

    check for invalid qrcodes, print data on res page

--- projeto_cm_flutter/lib/main.dart
+++ projeto_cm_flutter/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:projeto_cm_flutter/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  await dotenv.load(fileName: ".env");
+  
   runApp(const MyApp());
 }
 
--- projeto_cm_flutter/lib/screens/scan_qr_code_screen.dart
+++ projeto_cm_flutter/lib/screens/scan_qr_code_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:projeto_cm_flutter/screens/result_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ScanQRCodeScreen extends StatefulWidget {
   const ScanQRCodeScreen({Key? key}) : super(key: key);
@@ -13,28 +14,65 @@ class _ScanQRCodeScreenState extends State<ScanQRCodeScreen> {
   MobileScannerController cameraController = MobileScannerController();
   bool _screenOpened = false;
   bool isTorchOn = false;
+  bool _isDialogOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    dotenv.load();
+  }
 
   void _foundBarcode(BarcodeCapture barcodeCapture) {
     final List<Barcode> barcodes = barcodeCapture.barcodes;
     if (!_screenOpened && barcodes.isNotEmpty) {
       final String code = barcodes.first.rawValue ?? '---';
       debugPrint('Barcode found! $code');
-      _screenOpened = true;
+      final String apiUrl = dotenv.env['API_URL'] ?? '';
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ResultScreen(
-            code: code,
-            screenClosed: _screenWasClosed,
+      // Validate if the scanned code starts with the API_URL
+      if (code.startsWith(apiUrl)) {
+        _screenOpened = true;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResultScreen(
+              code: code,
+              screenClosed: _screenWasClosed,
+            ),
           ),
-        ),
-      );
+        );
+      } else if (!_isDialogOpen) {
+        // Show error if the scanned code doesn't match the API_URL and no dialog is open
+        _showInvalidUrlDialog();
+      }
     }
   }
 
   void _screenWasClosed() {
     _screenOpened = false;
+  }
+
+  void _showInvalidUrlDialog() {
+    _isDialogOpen = true; 
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Invalid QR Code"),
+          content: const Text(
+              "The scanned QR code is invalid. Please scan a valid URL."),
+          actions: [
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                _isDialogOpen = false;
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
