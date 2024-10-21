aeafb0fcc13b0fccd208b890a2e030815450877f
Gui <guicostaantunes@gmail.com>
2024-10-21 17:10:26 +0100

    Map Offline, GPS and Internet Connections

--- projeto_cm_flutter/lib/main.dart
+++ projeto_cm_flutter/lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 
 import 'package:projeto_cm_flutter/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await FMTCObjectBoxBackend().initialise();
+  await FMTCStore('busMap').manage.create();
 
   runApp(const MyApp());
 }
--- projeto_cm_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ projeto_cm_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_core
 import flutter_secure_storage_macos
 import local_auth_darwin
 import mobile_scanner
+import objectbox_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
